cdb7eae0185227a61818bef6446e5efbd8b10b18
bensvarnum <bens.varnum@gmail.com>
2021-03-19 18:09:19 -0400

    npx react app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <h1>app</h1>
+    </>
+  );
+};
+
+export default App;
